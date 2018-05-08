8542291fa780998741cf16f48b5b202eb9979420
sjaimo <sjaimo@gmail.com>
2018-05-08 17:26:01 +0900

    second commit

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
-# Create your views here.
+# 글목록 조회
 def post_list(request):
     #name = 'Django~~'
     #return HttpResponse('''<h1>Hello {myname}</h1>'''.format(myname=name))
@@ -11,4 +11,9 @@ def post_list(request):
     #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     posts = Post.objects.all().order_by('published_date')
     return render(request,'blog/post_list.html',{'posts':posts})
+
+# 글상세조회
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blog/post_detail.html',{'post':post})
 
